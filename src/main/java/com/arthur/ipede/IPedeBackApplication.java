3b266376c5f39368458a7ee0940d4996c099cd2e
Arthur Marques <arthurmarques@localhost.localdomain>
2018-09-24 22:35:04 -0300

    Projeto criado

---
+++ src/main/java/com/arthur/ipede/IPedeBackApplication.java
@@ -0,0 +1,12 @@
+package com.arthur.ipede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPedeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPedeBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arthur/ipede/IPedeBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arthur.ipede;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IPedeBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
